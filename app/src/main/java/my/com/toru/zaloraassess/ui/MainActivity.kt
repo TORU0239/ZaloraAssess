4a8d48ff69606f241cd8efeee2b52412bf64161c
Toru Choi <toru.choi@11street.my>
2018-11-27 18:01:40 +0800

    initialization for MVVM pattern

--- app/src/main/java/my/com/toru/zaloraassess/ui/MainActivity.kt
+++ app/src/main/java/my/com/toru/zaloraassess/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package my.com.toru.zaloraassess.ui
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import my.com.toru.zaloraassess.R
@@ -9,5 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
     }
 }
