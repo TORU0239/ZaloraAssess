5eaa03f4e4d9a6bbf9a715cce584077d32dcfd5d
Toru Choi <toru.choi@11street.my>
2018-11-28 17:33:07 +0800

    prototype for concurrent transmission

--- app/src/main/java/my/com/toru/zaloraassess/viewmodel/MainViewModel.kt
+++ app/src/main/java/my/com/toru/zaloraassess/viewmodel/MainViewModel.kt
@@ -1,14 +1,16 @@
 package my.com.toru.zaloraassess.viewmodel
 
-import android.arch.lifecycle.MutableLiveData
+import android.annotation.SuppressLint
 import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
+import android.os.Handler
+import android.os.Message
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
-import android.widget.Toast
 import my.com.toru.zaloraassess.Util.Util
+import my.com.toru.zaloraassess.local.MessageProcessor
 import my.com.toru.zaloraassess.ui.adapter.MainAdapter
 
 class MainViewModel : ViewModel() {
@@ -17,8 +19,6 @@ class MainViewModel : ViewModel() {
     }
 
     val msgFromUser = ObservableField<String>()
-    val msgLiveData = MutableLiveData<String>()
-
     val adapter:MainAdapter = MainAdapter()
 
     val editorActionListener = TextView.OnEditorActionListener { v, actionId, _ ->
@@ -33,14 +33,48 @@ class MainViewModel : ViewModel() {
         }
     }
 
-    fun onClickButton(view: View){
-        Toast.makeText(view.context, "test:: ${msgFromUser.get()}", Toast.LENGTH_SHORT).show()
-//        msgLiveData.value = msgFromUser.get()
+    private val handler = AbsHandler()
 
+    fun onClickButton(view: View) {
         msgFromUser.get()?.filter {
             it.toString() != ""
         }?.let {
-            adapter.setData(it)
+            if(it.length > 50){
+                sendingMessageConcurrently(MessageProcessor.splitingMessage(it))
+            }
+            else{
+                adapter.setData(it)
+            }
+        }
+    }
+
+    private fun sendingMessageConcurrently(msgList:ArrayList<String>){
+        handler.msgList = msgList
+        val message = Message()
+        with(message){
+            what = 1004
+            arg1 = 0
+        }
+        handler.sendMessage(message)
+    }
+
+    @SuppressLint("HandlerLeak")
+    inner class AbsHandler : Handler(){
+        var msgList = ArrayList<String>()
+        override fun handleMessage(msg: Message?) {
+            when(msg?.what){
+                1004 ->{
+                    if(msg.arg1 != msgList.size){
+                        adapter.setData(msgList[msg.arg1])
+                        val newMSg = Message()
+                        newMSg.what = 1004
+                        newMSg.arg1 = (msg.arg1 + 1)
+                        Log.w(TAG, "msg.arg1: ${msg.arg1}")
+                        sendMessageDelayed(newMSg, 1000)
+                    }
+                }
+                else->{}
+            }
         }
     }
 }
